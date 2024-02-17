2cdbcddfc5a04ae7b2f5051b0f67be5cae168d3b
wirabudhi <bpantaiselatan@gmail.com>
2024-02-17 21:46:19 +0800

    Add toast notification and delete functionality to KeranjangTable

--- app/Livewire/KeranjangTable.php
+++ app/Livewire/KeranjangTable.php
@@ -5,8 +5,10 @@ namespace App\Livewire;
 use App\Models\Keranjang;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Builder;
+use Masmerise\Toaster\Toastable;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Detail;
 use PowerComponents\LivewirePowerGrid\Exportable;
 use PowerComponents\LivewirePowerGrid\Facades\Filter;
 use PowerComponents\LivewirePowerGrid\Footer;
@@ -19,6 +21,7 @@ use PowerComponents\LivewirePowerGrid\Traits\WithExport;
 final class KeranjangTable extends PowerGridComponent
 {
     use WithExport;
+    use Toastable;
 
     public function setUp(): array
     {
@@ -32,6 +35,9 @@ final class KeranjangTable extends PowerGridComponent
             Footer::make()
                 ->showPerPage()
                 ->showRecordCount(),
+            Detail::make()
+                ->view('details.keranjang-detail')
+                ->showCollapseIcon(),
         ];
     }
 
@@ -91,11 +97,32 @@ final class KeranjangTable extends PowerGridComponent
     public function actions(\App\Models\Keranjang $row): array
     {
         return [
-            Button::add('edit')
-                ->slot('Edit: ' . $row->id)
+            Button::add('delete')
+                ->slot('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                ')
                 ->id()
                 ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
-                ->dispatch('edit', ['rowId' => $row->id])
+                ->dispatch('delete', ['rowId' => $row->id])
         ];
+    }
+
+    protected function getListeners()
+    {
+        return array_merge(
+            parent::getListeners(),
+            [
+                'delete',
+            ]
+        );
+    }
+
+    // Function to delete data
+    public function delete($rowId)
+    {
+        $keranjang = Keranjang::findOrFail($rowId);
+        $keranjang->delete();
+        $this->success('Data keranjang berhasil dihapus');
     }
 }
