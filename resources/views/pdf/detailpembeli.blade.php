db6cff4e93f4c298cadf7cdf3baa582cbe9a5cf3
wirabudhi <bpantaiselatan@gmail.com>
2024-03-13 12:54:28 +0800

    Fix user policy and update detail pembeli table layout

--- resources/views/pdf/detailpembeli.blade.php
+++ resources/views/pdf/detailpembeli.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h2>  Detail Pembeli Table</h2>
+    <h2> Detail Pembeli Table</h2>
     <table style="width: 100%; border-collapse: collapse;">
         <thead>
             <tr>
@@ -7,19 +7,17 @@
                 <th style="border: 1px solid; padding: 8px;">Nama Pembeli</th>
                 <th style="border: 1px solid; padding: 8px;">Alamat</th>
                 <th style="border: 1px solid; padding: 8px;">No Telp</th>
-                <th style="border: 1px solid; padding: 8px;">Tanggal transaksi </th>
             </tr>
         </thead>
 
         <tbody>
             @foreach ($datasource as $detailpembeli)
-            <tr>
-                <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->id }}</td>
-                <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->name }}</td>
-                <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->alamat }}</td>
-                <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->no_wa }}</td>
-                <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->tanggaltransaksi_teraakhir }}</td>
-            </tr>
+                <tr>
+                    <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->id }}</td>
+                    <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->name }}</td>
+                    <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->alamat }}</td>
+                    <td style="border: 1px solid; padding: 8px;">{{ $detailpembeli->no_wa }}</td>
+                </tr>
             @endforeach
         </tbody>
     </table>
