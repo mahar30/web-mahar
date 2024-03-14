6bb588626b8f02badb9cd1abe13aea71afb412c1
wirabudhi <bpantaiselatan@gmail.com>
2024-03-14 13:20:53 +0800

    Update gallery and transaction views

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,7 +6,7 @@
             <div class="text-xl">
                 <h1 class="text-center text-4xl font-bold mt-10">Galeri</h1>
             </div>
-            <div class="flex justify-center mt-5 carousel carousel-center rounded-box">
+            {{-- <div class="flex justify-center mt-5 carousel carousel-center rounded-box">
                 <div class="carousel-item">
                     <img src="{{ asset('storage/foto-gallery/1.jpg') }}" alt="Drink" class="object-cover h-96" />
                 </div>
@@ -19,7 +19,36 @@
                 <div class="carousel-item">
                     <img src="{{ asset('storage/foto-gallery/4.jpg') }}" alt="Drink" class="object-cover h-96" />
                 </div>
+            </div> --}}
+
+            <div class="flex justify-center my-5 flex-wrap ">
+                <div class="card w-96 bg-base-100 shadow-xl m-5">
+                    <figure class="p-1">
+                        <img src="{{ asset('storage/foto-gallery/9.jpg') }}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl m-5">
+                    <figure class="p-1">
+                        <img src="{{ asset('storage/foto-gallery/10.jpg') }}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl m-5">
+                    <figure class="p-1">
+                        <img src="{{ asset('storage/foto-gallery/11.jpg') }}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl m-5">
+                    <figure class="p-1">
+                        <img src="{{ asset('storage/foto-gallery/12.jpg') }}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl m-5">
+                    <figure class="p-1">
+                        <img src="{{ asset('storage/foto-gallery/13.jpg') }}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                </div>
             </div>
+
         </div>
         @livewire('footer')
     </div>
--- resources/views/pdf/transaksi.blade.php
+++ resources/views/pdf/transaksi.blade.php
@@ -6,10 +6,7 @@
                 <th style="border: 1px solid #000; padding: 8px;"> Id </th>
                 <th style="border: 1px solid #000; padding: 8px;"> Nama </th>
                 {{-- <th style="border: 1px solid #000; padding: 8px;"> Kode Transaksi </th> --}}
-                <th style="border: 1px solid #000; padding: 8px;"> Total Harga </th>
-                <th style="border: 1px solid #000; padding: 8px;"> No-Wa Pembeli </th>
-                <th style="border: 1px solid #000; padding: 8px;"> Tipe Pembayaran</th>
-                <th style="border: 1px solid #000; padding: 8px;"> Total Pembelian </th>
+                <th style="border: 1px solid #000; padding: 8px;"> Total Harga </th>>
                 <th style="border: 1px solid #000; padding: 8px;"> Status </th>
                 
             </tr>
@@ -21,9 +18,6 @@
                 <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->name }} </td>
                 {{-- <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->kode_transaksi }} </td> --}}
                 <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->total_harga }} </td>
-                <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->no_wa_pembeli }} </td>
-                <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->tipe_pembayaran }} </td>
-                <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->total_pembelian }} </td>
                 <td style="border: 1px solid #000; padding: 8px;"> {{ $transaksi->status }} </td>
             </tr>
             @endforeach
