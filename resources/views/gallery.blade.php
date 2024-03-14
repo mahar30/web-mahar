6f1abd44dd1588f10554d34bbfe7240a9a1fede0
wirabudhi <bpantaiselatan@gmail.com>
2024-03-14 21:16:10 +0800

    Remove commented out carousel code in gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -6,21 +6,6 @@
             <div class="text-xl">
                 <h1 class="text-center text-4xl font-bold mt-10">Galeri</h1>
             </div>
-            {{-- <div class="flex justify-center mt-5 carousel carousel-center rounded-box">
-                <div class="carousel-item">
-                    <img src="{{ asset('storage/foto-gallery/1.jpg') }}" alt="Drink" class="object-cover h-96" />
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('storage/foto-gallery/2.jpg') }}" alt="Drink" class="object-cover h-96" />
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('storage/foto-gallery/3.jpg') }}" alt="Drink" class="object-cover h-96" />
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('storage/foto-gallery/4.jpg') }}" alt="Drink" class="object-cover h-96" />
-                </div>
-            </div> --}}
-
             <div class="flex justify-center my-5 flex-wrap ">
                 <div class="card w-96 bg-base-100 shadow-xl m-5">
                     <figure class="p-1">
